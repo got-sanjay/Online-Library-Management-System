fa6b6c6f7760dcf3a7755eeb599f51339cecfe46
Sanjay Kumar <144806035+got-sanjay@users.noreply.github.com>
2025-04-11 10:10:16 +0530

    UPIR : Upload Project In Repo

---
+++ hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>Libray Management System &nbsp <a href=""></strong>
+  </footer>
